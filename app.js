a7972226406cb87727d34ddea5daaec0de39d54b
GeorgessDavid <georgessdavid@gmail.com>
2022-09-08 20:47:00 -0300

    Funcionalidades de express para el localhost agregada.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 let medicos = require("./datos/medicos");
 
 let {agregarMedicos, borrarMedico, buscador} = require("./funciones/functions");
@@ -9,4 +10,50 @@ let {agregarMedicos, borrarMedico, buscador} = require("./funciones/functions");
 
 console.log(buscador())
 
+*/
+const express = require('express');
 
+const app = express();
+
+const path = require('path');
+
+const publicPath = path.join(__dirname, '/public');
+
+app.listen(3002, (req, res) => {
+
+    console.log("Servidor activo - Puerto: " + 3002)
+
+}
+);
+
+app.use(express.static((__dirname + '/public')));
+
+app.get('/', (req,res) => {
+
+    res.sendFile((__dirname + '/views/index.html'));
+
+})
+
+app.get('/login', (req, res) => {
+
+    res.sendFile((__dirname + '/views/login.html'));
+
+})
+
+app.get('/register', (req, res) => {
+
+    res.sendFile((__dirname + '/views/registro.html'));
+
+})
+
+app.get('/turnos', (req, res) => {
+
+    res.sendFile((__dirname + '/views/turnos.html'));
+
+})
+
+app.get('/historiaClinica', (req, res) => {
+
+    res.sendFile((__dirname + '/views/historiaClinica.html'))
+
+})
