33e7639a476b9b228bdc78848d32538a6321e6d8
mariedonna <mariela.donnoli@gmail.com>
2022-09-15 15:30:19 -0300

    para seguir codeando

--- app.js
+++ app.js
@@ -16,15 +16,27 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-
+/*
 const publicPath = path.join(__dirname, '/public');
-
+*/
 app.listen(3002, (req, res) => {
 
     console.log("Servidor activo - Puerto: " + 3002)
 
 }
 );
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname, '/views/index.html' )));
+
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname, '/views/turnos.html' )));
+
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname, '/views/registro.html' )));
+/*
 
 app.use(express.static((__dirname + '/public')));
 
@@ -57,3 +69,4 @@ app.get('/historiaClinica', (req, res) => {
     res.sendFile((__dirname + '/views/historiaClinica.html'))
 
 })
+*/
