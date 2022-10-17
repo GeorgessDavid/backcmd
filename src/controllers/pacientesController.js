60cc6ab9a57c763e55712144478148bb5965e3cb
Martin <martin@finalis.com>
2022-10-17 10:20:22 -0300

    unifico todo users -> pacientes

---
+++ src/controllers/pacientesController.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const fs = require('fs');
+
+const pacientesFilePath = path.join(__dirname, '../../datos/pacientes.json');
+const pacientes = JSON.parse(fs.readFileSync(pacientesFilePath, 'utf-8'));
+const bcrypt = require('bcryptjs');
+
+
+const controlador = {
+    login: (req,res) => {
+        res.render("login")
+    },
+    register: (req,res) => {
+        res.render("registro")
+    },
+    save: (req, res) => {
+        let id = pacientes[pacientes.length-1].id + 1;
+        let paciente = {
+            id: id,
+            usuario: req.body.usuario,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password,10),
+        }
+
+        pacientes.push(paciente);
+        fs.writeFileSync(pacientesFilePath,JSON.stringify(pacientes,null," "));
+
+        res.redirect('/');
+    },
+    index: (req,res) => {
+        res.render("pacientes",{ps: pacientes});
+    },
+    detallePaciente: (req,res) => {
+        let idPaciente = req.params.id;
+        let objPaciente;
+
+        for (let o of pacientes){
+            if (idPaciente == o.id){
+                objPaciente=o;
+                break;
+            }
+        }
+        res.render('editarPaciente',{paciente: objPaciente})
+    },
+    editarPaciente: (req,res) => {
+        let id = req.params.id
+        for (let i = 0; i < pacientes.length; i++) {
+            if (pacientes[i].id == id) {
+                pacientes[i].usuario = req.body.usuario;
+                pacientes[i].nombre = req.body.nombre
+                pacientes[i].apellido = req.body.apellido
+                pacientes[i].email = req.body.email
+                pacientes[i].password = req.body.password
+            }
+        }
+
+        fs.writeFileSync(pacientesFilePath,JSON.stringify(pacientes,null," "));
+
+        res.redirect('/pacientes');
+    },
+
+    delete: (req, res) => {
+
+        let id = req.params.id
+        let filteredPacientes = pacientes.filter(paciente => paciente.id != id)
+
+        fs.writeFileSync(pacientesFilePath,JSON.stringify(filteredPacientes,null," "));
+
+        res.redirect('/');
+    }
+}
+
+module.exports = controlador
