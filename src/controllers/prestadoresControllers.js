1a0f65f9ecc1a11b2d5d41f50240378e52b6af0b
GeorgessDavid <georgessdavid@gmail.com>
2022-10-21 05:55:34 -0300

    funcionalidades editar y eliminar agregadas correctamente

--- src/controllers/prestadoresControllers.js
+++ src/controllers/prestadoresControllers.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
 const path = require('path')
 const multer = require('multer');
 const userDatabase = require('../../datos/innerDatabase.json')
 const session = require('express-session')
-const publicMedicos = require("../../datos/publicMedicos.json");
-const fs = require("fs");
+const prestadoresFilePath = path.join(__dirname, '../../datos/publicMedicos.json')
+const publicMedicos = JSON.parse(fs.readFileSync(prestadoresFilePath, 'utf-8'))
+
 
 const prestadoresController = {
     index: (req, res) => {
@@ -54,7 +56,7 @@ const prestadoresController = {
 
         res.redirect("/prestadores/home")
     },
-    detallePrestador: (req, res) => {
+    editandoPrestador: (req, res) => {
         let idPrestador = req.params.id;
         let objPrestador;
 
@@ -66,6 +68,57 @@ const prestadoresController = {
         }
         res.render('prestadoresViews/editarPrestador', { prestador: objPrestador })
     },
+    
+    confirmarEliminacion: (req, res) =>{
+        let idPrestador = req.params.id;
+        let objPrestador;
+        
+        for (let o of publicMedicos) {
+            if (idPrestador == o.id) {
+                objPrestador = o;
+                break;
+            }
+        }
+
+        res.render('prestadoresViews/confirmDelete', {prestador: objPrestador})
+    },
+
+    deletePrestador: (req, res) => {
+
+        let id = req.params.id
+        let filteredPrestadores = publicMedicos.filter(prestador => prestador.id != id)
+
+        fs.writeFileSync(prestadoresFilePath,JSON.stringify(filteredPrestadores,null," "));
+
+        res.redirect('/prestadores/eliminacionConfirmada');
+    },
+    
+    eliminacionConfirmada: (req, res) =>{
+        res.render('prestadoresViews/eliminacionConfirmada')
+    },
+
+    editarPrestador: (req,res) =>{
+        let id = req.params.id
+        for (let i = 0; i < publicMedicos.length; i++) {
+            if (publicMedicos[i].id == id) {
+                publicMedicos[i].nombre = req.body.nombre;
+                publicMedicos[i].apellido = req.body.apellido
+                publicMedicos[i].especialidad = req.body.especialidad
+                publicMedicos[i].especialidad2 = req.body.especialidad2
+                publicMedicos[i].sexo = req.body.sexo
+                publicMedicos[i].estudios = req.body.estudios
+                if(req.file){
+                    publicMedicos[i].profileImg = req.file.filename
+                }else{
+                    publicMedicos[i].profileImg = "default_profile_img.png"
+                }
+            }
+        }
+
+        fs.writeFileSync(prestadoresFilePath,JSON.stringify(publicMedicos,null," "));
+
+        res.redirect('/prestadores/home');
+    }
 }
 
 module.exports = prestadoresController;
--- src/routes/prestadoresRoutes.js
+++ src/routes/prestadoresRoutes.js
@@ -27,10 +27,17 @@ router.post("/login", prestadoresController.login)
 
 router.get("/home", prestadoresController.home)
 
-router.get("/home/:id", prestadoresController.detallePrestador)
+router.get("/editandoPrestador/:id", prestadoresController.editandoPrestador)
 
 router.get("/agregarMedico", prestadoresController.agregarMedico)
 
 router.post("/agregarMedico", uploadFile.single('profileImg'), prestadoresController.agregarMedicoPublico)
 
+router.get("/home/confirmDelete/:id", prestadoresController.confirmarEliminacion);
+
+router.delete("/home/confirmDelete/:id", prestadoresController.deletePrestador);
+
+router.get("/eliminacionConfirmada", prestadoresController.eliminacionConfirmada)
+
+router.put("/editarPrestador/:id", uploadFile.single('profileImg'), prestadoresController.editarPrestador)
 module.exports = router;
