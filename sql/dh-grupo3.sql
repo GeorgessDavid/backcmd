d0ffa851e4e44b84cecf13f80907f14670db165f
GeorgessDavid <georgessdavid@gmail.com>
2022-11-05 01:45:03 -0300

    SCRIPT DATABASE ACTUALIZADO

--- sql/dh-grupo3.sql
+++ sql/dh-grupo3.sql
@@ -1,110 +1,199 @@
-CREATE DATABASE `dh-grupo3`;
+-- CREACIÓN DE BASE DE DATOS
+
+CREATE DATABASE Clinica_db; 
+USE Clinica_db;
+
+-- CREACIÓN DE TABLAS 
 
 CREATE TABLE `Usuario` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL,
-  `apellido` VARCHAR(30) NOT NULL ,
-  `dni` VARCHAR(30) NOT NULL ,
-  `email` VARCHAR(30) NOT NULL ,
-  `clave` VARCHAR(30) NOT NULL ,
-  `matricula` VARCHAR(30) ,
-  `sexo` BIT NOT NULL ,
-  `domicilio` VARCHAR(30) NOT NULL ,
-  `telefono` VARCHAR(30),
-  `Obra_Social_id` INT,
-  `Rol_id` INT NOT NULL ,
+  `id` INT NOT NULL,
+  `alias` varchar(30) NOT NULL ,
+  `nombre` varchar(30) NOT NULL ,
+  `apellido` varchar(30) NOT NULL ,
+  `dni` varchar(12) NOT NULL ,
+  `email` varchar(50) NOT NULL ,
+  `clave` varchar(500) NOT NULL ,
+  `matricula` varchar(10) ,
+  `sexo` bit NOT NULL ,
+  `domicilio` varchar(50) NOT NULL ,
+  `telefono` varchar(20) NOT NULL ,
+  `imagen` varchar(80) ,
+  `Obra_Social_id` INT ,
+  `Rol_id` INT NOT NULL , 
  PRIMARY KEY (`id`)
-);
+)
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Rol` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL ,
+  `id`  INT NOT NULL ,
+  `nombre` varchar(70) NOT NULL , 
  PRIMARY KEY (`id`)
-);
+)
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Especialidad` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL ,
+  `id`  INT NOT NULL ,
+  `nombre` varchar(30) NOT NULL , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Profesional_Especialidad` (
-  `id` INT AUTO_INCREMENT NOT NULL,
+  `id`  INT NOT NULL ,
   `Profesional_id` INT NOT NULL ,
-  `Especialidad_id` INT NOT NULL ,
+  `Especialidad_id` INT NOT NULL , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Tramiento` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL ,
+  `id`  INT NOT NULL ,
+  `nombre` varchar(30) NOT NULL , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Profesional_Tratamiento` (
-  `id` INT AUTO_INCREMENT NOT NULL,
+  `id`  INT NOT NULL ,
   `Profesional_id` INT NOT NULL ,
-  `Tratamiento_id` INT NOT NULL ,
+  `Tratamiento_id` INT NOT NULL , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
 CREATE TABLE `Turno` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `Paciente_id` INT NOT NULL,
-  `Profesional_id` INT NOT NULL,
-  `fecha_creacion` DATETIME NOT NULL,
-  `fecha_cancelacion` DATETIME,
-  `fecha_turno` DATETIME NOT NULL,
-  `Tratamiendo_id` INT,
- PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Planilla_horaria` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `dia_semana` VARCHAR(30) NOT NULL ,
-  `hora_inicio` VARCHAR(30) NOT NULL ,
-  `hora_fin` VARCHAR(30) NOT NULL ,
-  `Profesional_id` INT NOT NULL ,
- PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Diagnostico` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `descripcion` longtext NOT NULL ,
-  `fecha` DATETIME NOT NULL ,
-  `nombre_adjunto` VARCHAR(30) ,
+  `id`  INT NOT NULL ,
   `Paciente_id` INT NOT NULL ,
   `Profesional_id` INT NOT NULL ,
-  `Turno_id` INT,
+  `fecha_creacion` datetime NOT NULL ,
+  `fecha_cancelacion` datetime ,
+  `fecha_turno` datetime NOT NULL ,
+  `Tratamiento_id` INT , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
+ 
 
-CREATE TABLE `Obras_sociales_adheridas` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL ,
-  `Obra_social_id` INT NOT NULL ,
+CREATE TABLE `Planilla_Horaria` (
+  `id`  INT NOT NULL ,
+  `dia_semana` varchar(50) NOT NULL ,
+  `hora_inicio` varchar(50) NOT NULL ,
+  `hora_fin` varchar(50) NOT NULL ,
+  `Profesional_id` INT NOT NULL , 
+ PRIMARY KEY (`id`)
+) 
+COLLATE='utf8mb4_general_ci'
+;
+
+CREATE TABLE `Diagnostico` (
+  `id`  INT NOT NULL ,
+  `descripcion` text NOT NULL ,
+  `fecha` datetime NOT NULL ,
+  `nombre_adjunto` varchar(50) ,
+  `Paciente_id` INT NOT NULL ,
   `Profesional_id` INT NOT NULL ,
+  `Turno_id` INT , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
-CREATE TABLE `Obra_social` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(30) NOT NULL ,
+CREATE TABLE `Obra_Social_Adherida` (
+  `id`  INT NOT NULL ,
+  `nombre` varchar(60) NOT NULL ,
+  `Obra_Social_id` INT NOT NULL ,
+  `Profesional_id` INT NOT NULL , 
  PRIMARY KEY (`id`)
-);
+) 
+COLLATE='utf8mb4_general_ci'
+;
 
-ALTER TABLE `Usuario` ADD FOREIGN KEY (Obra_Social_id) REFERENCES `Obra_social` (`id`);
+CREATE TABLE `Obra_Social` (
+  `id`  INT NOT NULL ,
+  `nombre` varchar(80) NOT NULL , 
+ PRIMARY KEY (`id`)
+) 
+COLLATE='utf8mb4_general_ci'
+;
+ 
+
+CREATE TABLE `Clinica` (
+  `id`  INT NOT NULL ,
+  `nombre` varchar(60) NOT NULL ,
+  `direccion` varchar(50) NOT NULL , 
+ PRIMARY KEY (`id`)
+) 
+COLLATE='utf8mb4_general_ci'
+;
+
+CREATE TABLE `Trabajador_Clinica` (
+  `id`  INT NOT NULL ,
+  `fechaInicio` datetime NOT NULL ,
+  `fechaFin` datetime ,
+  `Usuario_id` INT ,
+  `Clinica_id` INT , 
+ PRIMARY KEY (`id`)
+) 
+COLLATE='utf8mb4_general_ci'
+;
+ 
+
+CREATE TABLE `Registro_Caja` (
+  `id`  INT NOT NULL ,
+  `descripcion` text ,
+  `ingreso` bit NOT NULL ,
+  `monto` decimal ,
+  `Usuario_id` INT ,
+  `Clinica_id` INT , 
+ PRIMARY KEY (`id`)
+) 
+COLLATE='utf8mb4_general_ci'
+;
+
+
+-- RELACION FK 
+
+ALTER TABLE `Usuario` ADD FOREIGN KEY (Obra_Social_id) REFERENCES `Obra_Social` (`id`);
+				
 ALTER TABLE `Usuario` ADD FOREIGN KEY (Rol_id) REFERENCES `Rol` (`id`);
+				
 ALTER TABLE `Profesional_Especialidad` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Profesional_Especialidad` ADD FOREIGN KEY (Especialidad_id) REFERENCES `Especialidad` (`id`);
+				
 ALTER TABLE `Profesional_Tratamiento` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Profesional_Tratamiento` ADD FOREIGN KEY (Tratamiento_id) REFERENCES `Tramiento` (`id`);
+				
 ALTER TABLE `Turno` ADD FOREIGN KEY (Paciente_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Turno` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
-ALTER TABLE `Turno` ADD FOREIGN KEY (Tratamiendo_id) REFERENCES `Tramiento` (`id`);
-ALTER TABLE `Planilla_horaria` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
+ALTER TABLE `Turno` ADD FOREIGN KEY (Tratamiento_id) REFERENCES `Tramiento` (`id`);
+				
+ALTER TABLE `Planilla_Horaria` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Diagnostico` ADD FOREIGN KEY (Paciente_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Diagnostico` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
 ALTER TABLE `Diagnostico` ADD FOREIGN KEY (Turno_id) REFERENCES `Turno` (`id`);
-ALTER TABLE `Obras_sociales_adheridas` ADD FOREIGN KEY (Obra_social_id) REFERENCES `Obra_social` (`id`);
-ALTER TABLE `Obras_sociales_adheridas` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
+ALTER TABLE `Obra_Social_Adherida` ADD FOREIGN KEY (Obra_Social_id) REFERENCES `Obra_Social` (`id`);
+				
+ALTER TABLE `Obra_Social_Adherida` ADD FOREIGN KEY (Profesional_id) REFERENCES `Usuario` (`id`);
+				
+ALTER TABLE `Trabajador_Clinica` ADD FOREIGN KEY (Usuario_id) REFERENCES `Usuario` (`id`);
+				
+ALTER TABLE `Trabajador_Clinica` ADD FOREIGN KEY (Clinica_id) REFERENCES `Clinica` (`id`);
+				
+ALTER TABLE `Registro_Caja` ADD FOREIGN KEY (Usuario_id) REFERENCES `Usuario` (`id`);
+				
+ALTER TABLE `Registro_Caja` ADD FOREIGN KEY (Clinica_id) REFERENCES `Clinica` (`id`);
