21f211792d89eac7f48ded1d85c1ab89c4ace7a6
mariedonna <mariela.donnoli@gmail.com>
2022-11-16 20:42:50 -0300

    archivo config

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,11 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "password": "micontraseña",
+    "database": "cuchujero_okp",
+    "host": "dh-grupo3",
+    "dialect": "mysql",
+    "port": 3306
   },
   "test": {
     "username": "root",
