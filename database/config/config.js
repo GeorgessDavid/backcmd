d1b744a08dbb750713819eb83c025a1d7f9071a5
mariedonna <mariela.donnoli@gmail.com>
2022-11-22 18:13:52 -0300

    entornos casi configurado - configurar develop de config y el arch env

--- database/config/config.js
+++ database/config/config.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER, 
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE, 
-    "host": process.env.DB_HOST, 
-    "dialect": process.env.DB_DIALECT,
-    "port": process.env.DB_PORT,
+    "username": "root", 
+    "password": "micontraseña", 
+    "database": "dh-grupo3", 
+    "host": "127.0.0.1", 
+    "dialect": "mysql", 
+    "port": 3306  
   },
+
   "test": {
     "username": "root",
     "password": null,
@@ -25,11 +26,12 @@ module.exports = {
   }
 }
 
+//////////////// PARA CONFIGURAR ENTORNO TEST
 //"development": {
-  //"username": "root", 
-  //"password": "micontraseña", 
-  //"database": "dh-grupo3", 
-  //"host": "127.0.0.1", 
-  //"dialect": "mysql", 
-  //"port": 3306  
-//};
+//  "username": process.env.DB_USER, 
+//  "password": process.env.DB_PASSWORD,
+//  "database": process.env.DB_DATABASE, 
+//  "host": process.env.DB_HOST, 
+//  "dialect": process.env.DB_DIALECT,
+//  "port": process.env.DB_PORT,
+//},
