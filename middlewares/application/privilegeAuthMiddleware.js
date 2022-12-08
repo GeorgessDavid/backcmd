6005719267fd63e9cacd409efe6571af90b8386f
GeorgessDavid <georgessdavid@gmail.com>
2022-12-08 18:27:11 -0300

    access denied created.

--- middlewares/application/privilegeAuthMiddleware.js
+++ middlewares/application/privilegeAuthMiddleware.js
@@ -1,28 +1,28 @@
 let userTypeAuth = {
     admin: (req, res, next) => {
         if (req.session.usuario.Rol_id != 1) {
-            return res.redirect('/')
+            return res.render('error/accessDenied')
         }
 
         next();
     },
     secretaria: (req, res, next) => {
         if(req.session.usuario.Rol_id !=2) {
-            return res.redirect('/')
+            return res.render('error/accessDenied')
         };
 
         next();
     },
     medic: (req,res,next) => {
         if(req.session.usuario.Rol_id != 3){
-            return res.redirect('/')
+            return res.render('error/accessDenied')
         };
 
         next();
     },
     public: (req,res,next) => {
         if(req.session.usuario.Rol_id !=4){
-            return res.redirect('/')
+            return res.render('error/accessDenied')
         }
 
         next();
