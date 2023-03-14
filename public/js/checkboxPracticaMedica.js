60450d903b571461d106cbbcbbc12adecc5875c5
GeorgessDavid <georgessdavid@gmail.com>
2023-03-13 22:27:49 -0300

    practicas medicas para agregar usuario iniciado

---
+++ public/js/checkboxPracticaMedica.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+
+
+  var checkboxArea = document.getElementById("checkboxArea")
+
+
+  fetch('https://dh-grupo3.onrender.com/tratamientos/api').then(res => {
+    return res.json()
+  }).then(l => {
+    for (let i = 0; i < l.data.length; i++) {
+      const e = l.data[i];
+
+      let spinner = document.getElementById('loadingSpinner')
+
+            spinner.style.visibility = 'hidden';
+            spinner.style.opacity = '0';
+            spinner.classList.remove('m-5')
+
+      checkboxArea.innerHTML += `
+        <div class="col">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="${e.id}" id="flexCheckDefault ${i}">
+              <label class="form-check-label" for="flexCheckDefault" name="practicaMedica">
+                ${e.nombre}
+              </label>
+            </div>
+            <div class="form-check form-check-reverse">
+              <input class="form-check-input" type="checkbox" value="${l.data[i + 1].value}" id="reverseCheck1${i+1}" name="practicaMedica">
+              <label class="form-check-label" for="reverseCheck1">
+                ${l.data[i+1].nombre}
+              </label>
+            </div>
+        </div>`
+      i = i + 1
+    }
+  })
+})
