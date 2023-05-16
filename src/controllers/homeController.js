780fae7c4e7feaceeff7189a486ae85b81fc5373
GeorgessDavid <georgessdavid@gmail.com>
2023-05-16 02:48:26 -0300

    index visual changes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,22 +1,39 @@
 const path = require('path')
 const db = require('../../database/models')
 
-const controlador = { 
-    index: (req,res) => {
-        // prueba modelo clinica
-        db.Tratamiento.findAll().then((data)=>{
-            let datosEncontrados = [];
-            for ( let x of data){
-                let objetoDatoEncontrado = {
-                    id: x.id,
-                    nombre: x.nombre
-                }
-                datosEncontrados.push(objetoDatoEncontrado)
-            }
+
+
+const controlador = {
+    index: async (req, res) => {
+        const practicasMedicas = await db.Tratamiento.findAll()
+
+        let practicas = []
+
+        let color = ['f43838', '0863f6', '9800b3', '80800a', '0a8014', '80240a', '090051', '926100']
+
+
+        let data = []
+
+        let coloresDisponibles = [...color]; // Copiamos el array de colores disponibles
+
+        for (let i = 0; i < 8; i++) {
+            const indiceAleatorio = Math.floor(Math.random() * practicasMedicas.length);
+            const valorAleatorio = practicasMedicas[indiceAleatorio].nombre;
+
+            const colorIndexAleatorio = Math.floor(Math.random() * coloresDisponibles.length);
+            const colorAleatorio = coloresDisponibles.splice(colorIndexAleatorio, 1)[0];
+
+            data.push({
+                practica: valorAleatorio,
+                color: '#' + colorAleatorio
+            });
         }
-    )
-        res.render("index")
-    
+
+
+        console.log(data)
+
+        return res.render("index", { data: data })
+
     },
 }
 
